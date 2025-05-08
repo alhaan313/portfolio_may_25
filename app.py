3d951e6c45d08d639046e218a22f765df8c561b5
alhaan313 <a.md.alhaan@gmail.com>
2025-05-08 17:14:11 +0530

    vercel hosting

--- app.py
+++ app.py
@@ -58,5 +58,4 @@ def resume():
     return render_template('resume.html')
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
